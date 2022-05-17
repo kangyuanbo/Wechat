f62438bc3575aa35c481efaf7f78379abe284208
康渊博 <852906384@qq.com>
2022-05-17 17:35:07 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '422feaf125c28da9adc4622199d446fb88e4bf2a',
+    'name' => 'kangyuanbo/we-chat',
+  ),
+  'versions' => 
+  array (
+    'kangyuanbo/we-chat' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '422feaf125c28da9adc4622199d446fb88e4bf2a',
+    ),
+  ),
+);
